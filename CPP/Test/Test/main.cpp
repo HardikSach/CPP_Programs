9ac88209ab47febed9cc018ec77c26346863b0e5
HardikSach <108285347+HardikSach@users.noreply.github.com>
2022-06-27 20:33:52 +0530

    added a new file test.cpp

---
+++ CPP/Test/Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Amit Sachdeva on 27/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
