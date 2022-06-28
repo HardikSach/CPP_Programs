450852b6f975f141ece710d18731f190931b3cc1
HardikSach <108285347+HardikSach@users.noreply.github.com>
2022-06-28 11:51:43 +0530

    Added C++ code in 2D_Array_SpiralOrderMatrixTraversal.cpp

--- CPP/2D_Arrays/2D_Array-SpiralOrderMatrixTraversal/2D_Array-SpiralOrderMatrixTraversal/main.cpp
+++ CPP/2D_Arrays/2D_Array-SpiralOrderMatrixTraversal/2D_Array-SpiralOrderMatrixTraversal/main.cpp
@@ -10,7 +10,56 @@ using namespace std;
 
 int main() {
     
+    int n,m;
+    cin>>n>>m;
     
+    int array[n][m];
+    
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin>>array[i][j];
+        }
+    }
+    
+//    spral order print
+    
+    int rowStart =  0, rowEnd = n-1, columnStart = 0, columnEnd = m-1;
+    
+    while (rowStart <= rowEnd && columnStart <= columnEnd) {
+//        for rowStart
+        
+        for (int col = columnStart; col <= columnEnd; col++) {
+            cout << array[rowStart][col] << " ";
+        }
+        
+        rowStart++;
+        
+//        for columnEnd
+        
+        for (int row = rowStart; row <= rowEnd; row++) {
+            cout << array[row][columnEnd] << " ";
+        }
+        
+        columnEnd--;
+        
+//        for rowEnd
+        
+        for (int col = columnEnd; col >= columnStart; col--) {
+            cout << array[rowEnd][col] << " ";
+        }
+        
+        rowEnd--;
+        
+//        for columnStart
+        
+        for (int row = rowEnd; row >= rowStart; row--) {
+            cout << array[row][columnStart] << " ";
+        }
+        
+        columnStart++;
+    }
+    
+    cout << endl;
     
     return 0;
 }
