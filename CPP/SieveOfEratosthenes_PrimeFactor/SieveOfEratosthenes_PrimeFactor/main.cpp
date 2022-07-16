e64c1311c592014057838528c628f865cd440a4c
HardikSach <108285347+HardikSach@users.noreply.github.com>
2022-07-16 21:48:23 +0530

    Added a new C++ xcodeproject

---
+++ CPP/SieveOfEratosthenes_PrimeFactor/SieveOfEratosthenes_PrimeFactor/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  SieveOfEratosthenes_PrimeFactor
+//
+//  Created by Amit Sachdeva on 16/07/22.
+//
+
+#include <iostream>
+using namespace std;
+
+void primeFactor(int n) {
+    int spf[100] = {0};
+    
+    for (int i=2; i<=n; i++) {
+        spf[i] = i;
+    }
+    
+    for (int i=2; i<=n; i++) {
+        if (spf[i]==i) {
+            for (int j=i*i; j<=n; j+=i) {
+                if (spf[j]==j) {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+    
+    while (n!=1) {
+        cout<<spf[n]<<" ";
+        n /= spf[n];
+    }
+    
+    cout<<endl;
+}
+
+int main() {
+    
+    int n;
+    cin>>n;
+    
+    primeFactor(n);
+    
+    return 0;
+}
